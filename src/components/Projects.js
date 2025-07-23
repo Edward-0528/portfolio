13c7451749a324b2188dceabc7b989d8d5afb93e
Edward Granados <Alexanders.edward@gmail.com>
2025-07-22 21:02:33 -0700

    fixed issue with project list rendering

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import MinimalNetworkBackground from './MinimalNetworkBackground';
 import { projectsService } from '../lib/supabase';
 
